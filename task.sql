1a86ba15553a111f423670d66f13babfca40dc70
Orest Yakymiv <akimivorest@gmail.com>
2025-04-15 15:32:38 +0300

    fixed errors by testing

--- task.sql
+++ task.sql
@@ -7,17 +7,9 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    CountryID INT,
-	  FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
-);
 
 CREATE TABLE Product(	
-	ID INT,
+	  ID INT,
     ProductName VARCHAR (50),
     PRIMARY KEY (ID)
 );
@@ -26,9 +18,9 @@ CREATE TABLE ProductInfo(
 	  ID INT,
     ProductID INT,
     WarehouseAmount INT,
-    CountryID INT,
+    WarehouseID INT,
     FOREIGN KEY (ProductID) REFERENCES Product(ID) ON DELETE NO ACTION,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -36,38 +28,24 @@ CREATE TABLE Warehouse(
 		ID INT,
     WarehouseName VARCHAR(50),
     WarehouseAddress VARCHAR(50),
+    CountryID INT
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE WarehouseInfo(
-	  ID INT,
-    ProductID INT,
-    WarehouseAmount INT,
-    CountryID INT,
-    WarehouseID INT,
-    FOREIGN KEY (ProductID) REFERENCES ProductInventory(ID) ON DELETE NO ACTION,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
-);
-    
-INSERT INTO WarehouseInfo(ID,ProductID, WarehouseAmount,CountryID,WarehouseID)
-	VALUES(1, 1, 2, 1, 1);
-INSERT INTO WarehouseInfo(ID,ProductID, WarehouseAmount,CountryID,WarehouseID)
-	VALUES(2, 2, 5, 2, 2);
-    
+
+INSERT INTO Warehouse(ID, WarehouseName, WarehouseAddress, CountryID)
+	VALUES(1, 'Warehouse-1', 'City-1, Street-1', 1);
 INSERT INTO Warehouse(ID, WarehouseName, WarehouseAddress)
-	VALUES(1, 'Warehouse-1', 'City-1, Street-1');
-INSERT INTO Warehouse(ID, WarehouseName, WarehouseAddress)
-	VALUES(2, 'Warehouse-2', 'City-2, Street-2');
+	VALUES(2, 'Warehouse-2', 'City-2, Street-2', 2);
 
 INSERT INTO Product(ID,ProductName)
 	VALUES(1, 'AwersomeProduct');
 
 INSERT INTO ProductInfo(ID, ProductID, WarehouseAmount,CountryID)
-	VALUES(1, '1', 2, 1);
+	VALUES(1, 1, 2, 1);
 INSERT INTO ProductInfo(ID, ProductID, WarehouseAmount,CountryID)
-	VALUES(2, '2', 5, 2);
+	VALUES(2, 2, 5, 2);
 
 -- Populate test data
 
@@ -76,7 +54,4 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount, CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount, CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 2);
+
