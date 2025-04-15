4aa7f3a2c5873c85b17136f05a6e68d969e1a466
Orest Yakymiv <akimivorest@gmail.com>
2025-04-15 14:41:56 +0300

    Three normalization form

--- task.sql
+++ task.sql
@@ -1,5 +1,3 @@
--- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -19,6 +17,61 @@ CREATE TABLE ProductInventory (
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
+
+CREATE TABLE Product(	
+	ID INT,
+    ProductName VARCHAR (50),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInfo(
+	ID INT,
+    ProductID INT,
+    WarehouseAmount INT,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50), 
+    CountryID INT,
+    FOREIGN KEY (ProductID) REFERENCES ProductInventory(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Warehouse(
+		ID INT,
+        WarehouseName VARCHAR(50),
+        WarehouseAddress VARCHAR(50),
+        PRIMARY KEY (ID)
+);
+
+CREATE TABLE WarehouseInfo(
+	ID INT,
+    ProductID INT,
+    WarehouseAmount INT,
+    CountryID INT,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES ProductInventory(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+    
+INSERT INTO WarehouseInfo(ID,ProductID, WarehouseAmount,CountryID,WarehouseID)
+	VALUES(1, 1, 2, 1, 1);
+INSERT INTO WarehouseInfo(ID,ProductID, WarehouseAmount,CountryID,WarehouseID)
+	VALUES(2, 2, 5, 2, 2);
+    
+INSERT INTO Warehouse(ID, WarehouseName, WarehouseAddress)
+	VALUES(1, 'Warehouse-1', 'City-1, Street-1');
+INSERT INTO Warehouse(ID, WarehouseName, WarehouseAddress)
+	VALUES(2, 'Warehouse-2', 'City-2, Street-2');
+
+INSERT INTO Product(ID,ProductName)
+	VALUES(1, 'AwersomeProduct');
+
+INSERT INTO ProductInfo(ID, ProductID, WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
+	VALUES(1, '1', 2, 'Warehouse-1', 'City-1, Street-1',1);
+INSERT INTO ProductInfo(ID, ProductID, WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
+	VALUES(2, '2', 5, 'Warehouse-2', 'City-2, Street-2',2);
 
 -- Populate test data
 
